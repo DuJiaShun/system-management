7f5e384ecf59fbe6900b0d137c000108b599de55
dujiashun <15961756284@163.com>
2024-04-03 18:05:05 +0800

    change components.d.ts

--- src/types/components.d.ts
+++ src/types/components.d.ts
@@ -7,8 +7,25 @@ export {}
 
 declare module 'vue' {
   export interface GlobalComponents {
-    ElButton: typeof import('element-plus/es')['ElButton']
+    AppMain: typeof import('./../layout/components/AppMain.vue')['default']
+    ElBreadcrumb: typeof import('element-plus/es')['ElBreadcrumb']
+    ElBreadcrumbItem: typeof import('element-plus/es')['ElBreadcrumbItem']
+    ElDropdown: typeof import('element-plus/es')['ElDropdown']
+    ElDropdownItem: typeof import('element-plus/es')['ElDropdownItem']
+    ElDropdownMenu: typeof import('element-plus/es')['ElDropdownMenu']
+    ElMenu: typeof import('element-plus/es')['ElMenu']
+    ElMenuItem: typeof import('element-plus/es')['ElMenuItem']
+    ElOption: typeof import('element-plus/es')['ElOption']
+    ElScrollbar: typeof import('element-plus/es')['ElScrollbar']
+    ElSelect: typeof import('element-plus/es')['ElSelect']
+    ElSubMenu: typeof import('element-plus/es')['ElSubMenu']
+    Item: typeof import('./../layout/components/Sidebar/Item.vue')['default']
+    Link: typeof import('./../layout/components/Sidebar/Link.vue')['default']
+    Logo: typeof import('./../layout/components/Sidebar/Logo.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
+    Sidebar: typeof import('./../layout/components/Sidebar/index.vue')['default']
+    SidebarItem: typeof import('./../layout/components/Sidebar/SidebarItem.vue')['default']
+    SvgIcon: typeof import('./../components/SvgIcon/index.vue')['default']
   }
 }
