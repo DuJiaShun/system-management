77565f9ead12b7f0fb18dca78494be9d8e47e778
dujiashun <15961756284@163.com>
2024-04-07 16:35:06 +0800

    feat: 右上角菜单栏全屏功能

---
+++ src/layout/components/Navbar/Screenfull.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="screenfull" @click="toggle">
+    <svg-icon :icon-class="isFullscreen ? 'exit-fullscreen' : 'fullscreen'" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+defineOptions({ name: 'Screenfull' })
+const { isFullscreen, toggle } = useFullscreen()
+// 改写F11全屏
+window.onkeydown = function (event) {
+  if (event.code === 'F11') {
+    event.preventDefault()
+    toggle()
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.screenfull .svg-icon {
+  fill: #5a5e66;
+  width: 20px;
+  height: 20px;
+}
+</style>
--- src/layout/components/Navbar/index.vue
+++ src/layout/components/Navbar/index.vue
@@ -1,18 +1,13 @@
 <template>
   <div class="navbar">
-    <hamburger
-      id="hamburger-container"
-      :is-active="appStore.sidebar.opened"
-      class="hamburger-container"
-      @toggleClick="toggleClick"
-    />
-    <breadcrumb id="breadcrumb-container" class="breadcrumb-container" />
-
+    <hamburger class="hamburger-container" :is-active="appStore.sidebar.opened" @toggleClick="toggleClick" />
+    <breadcrumb class="breadcrumb-container" />
     <div class="right-menu">
       <template v-if="appStore.device !== 'mobile'">
+        <!-- 模糊搜索 -->
         <search id="header-search" class="right-menu-item" />
-        <!-- <error-log class="errLog-container right-menu-item hover-effect" /> -->
-        <!-- <screenfull id="screenfull" class="right-menu-item hover-effect" /> -->
+        <!-- 全屏 -->
+        <screenfull id="screenfull" class="right-menu-item hover-effect" />
         <!-- <el-tooltip content="Global Size" effect="dark" placement="bottom">
           <size-select id="size-select" class="right-menu-item hover-effect" />
         </el-tooltip> -->
@@ -20,23 +15,22 @@
       <el-dropdown class="avatar-container right-menu-item hover-effect" trigger="click">
         <div class="avatar-wrapper">
           <img :src="userStore.userinfo?.avatar + '?imageView2/1/w/80/h/80'" class="user-avatar" />
-          <span>{{ userStore.userinfo?.username }}</span>
-          <!-- <i class="el-icon-caret-bottom" /> -->
+          <span class="user-name">{{ userStore.userinfo?.username }}</span>
         </div>
-        <el-dropdown-menu slot="dropdown">
-          <router-link to="/profile/index">
-            <el-dropdown-item>个人设置</el-dropdown-item>
-          </router-link>
-          <a target="_blank" href="https://github.com/PanJiaChen/vue-element-admin/">
-            <el-dropdown-item>项目地址</el-dropdown-item>
-          </a>
-          <a target="_blank" href="https://panjiachen.github.io/vue-element-admin-site/#/">
-            <el-dropdown-item>项目文档</el-dropdown-item>
-          </a>
-          <el-dropdown-item divided @click.native="logout">
-            <span style="display: block">退出登录</span>
-          </el-dropdown-item>
-        </el-dropdown-menu>
+        <template #dropdown>
+          <el-dropdown-menu>
+            <router-link to="/profile/index">
+              <el-dropdown-item>个人设置</el-dropdown-item>
+            </router-link>
+            <a target="_blank" href="#">
+              <el-dropdown-item>项目地址</el-dropdown-item>
+            </a>
+            <a target="_blank" href="#">
+              <el-dropdown-item>项目文档</el-dropdown-item>
+            </a>
+            <el-dropdown-item divided @click.native="logout">退出登录</el-dropdown-item>
+          </el-dropdown-menu>
+        </template>
       </el-dropdown>
     </div>
   </div>
@@ -45,6 +39,8 @@
 <script lang="ts" setup>
 defineOptions({ name: 'Navbar' })
 import Breadcrumb from '@/components/Breadcrumb/index.vue'
+import Search from './Search.vue'
+import Screenfull from './Screenfull.vue'
 import { useAppStore, useUserStore } from '@/store'
 const appStore = useAppStore()
 const userStore = useUserStore()
@@ -82,23 +78,39 @@ function logout() {}
     float: right;
     height: 50px;
     line-height: 50px;
-    &:focus {
-      outline: none;
-    }
     .right-menu-item {
       display: inline-block;
       height: 100%;
       padding: 0 8px;
       font-size: 18px;
       color: #5a5e66;
-      vertical-align: text-bottom;
+      // vertical-align: text-bottom;
     }
-  }
-  .user-avatar {
-    cursor: pointer;
-    width: 40px;
-    height: 40px;
-    border-radius: 10px;
+    .hover-effect {
+      cursor: pointer;
+      transition: background 0.5s;
+      &:hover {
+        background: rgba(0, 0, 0, 0.025);
+      }
+    }
+    .avatar-container {
+      margin-right: 20px;
+      .avatar-wrapper {
+        height: 50px;
+        display: flex;
+        align-items: center;
+        .user-avatar {
+          cursor: pointer;
+          width: 40px;
+          height: 40px;
+          border-radius: 10px;
+        }
+        .user-name {
+          color: #000;
+          margin-left: 8px;
+        }
+      }
+    }
   }
 }
 </style>
